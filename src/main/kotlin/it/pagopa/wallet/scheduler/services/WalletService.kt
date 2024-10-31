21de95d679feac8654f84c5ba9370fc4871c00bd
Pietro Tota <pietro.tota@pagopa.it>
2024-10-31 17:19:46 +0100

    fix: verification metadata + spotless apply

--- src/main/kotlin/it/pagopa/wallet/scheduler/services/WalletService.kt
+++ src/main/kotlin/it/pagopa/wallet/scheduler/services/WalletService.kt
@@ -9,7 +9,6 @@ import java.util.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.Limit
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
--- src/test/kotlin/it/pagopa/wallet/scheduler/repositories/WalletRepositoryTest.kt
+++ src/test/kotlin/it/pagopa/wallet/scheduler/repositories/WalletRepositoryTest.kt
@@ -3,6 +3,9 @@ package it.pagopa.wallet.scheduler.repositories
 import it.pagopa.wallet.scheduler.WalletTestUtils
 import it.pagopa.wallet.scheduler.WalletTestUtils.WALLET_CREATED_STATUS
 import it.pagopa.wallet.scheduler.config.WalletSearchConfig
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,12 +13,7 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
-import java.time.Instant
-import java.time.temporal.ChronoUnit
-import kotlin.test.assertEquals
-
 
 @DataMongoTest
 @ExtendWith(SpringExtension::class)
@@ -30,24 +28,27 @@ class WalletRepositoryTest {
         val startDate: String = Instant.now().minus(10, ChronoUnit.MINUTES).toString()
         val endDate: String = Instant.now().plus(10, ChronoUnit.MINUTES).toString()
 
-        val saveMono = Flux.merge(
-            walletRepository.save(WalletTestUtils.walletDocument("CREATED")),
-            walletRepository.save(WalletTestUtils.walletDocument("CREATED"))
-        ).last()
+        val saveMono =
+            Flux.merge(
+                    walletRepository.save(WalletTestUtils.walletDocument("CREATED")),
+                    walletRepository.save(WalletTestUtils.walletDocument("CREATED"))
+                )
+                .last()
 
-        val walletsMono = saveMono.flatMap {
-            walletRepository.findByCreationDateBetweenAndStatusOrderByUpdateDateAsc(
-                startDate,
-                endDate,
-                WALLET_CREATED_STATUS,
-                walletSearchConfig.limit
-            ).collectList()
-        }
-
-        StepVerifier.create( walletsMono )
-            .assertNext { list ->
-                assertEquals(2, list.size)
+        val walletsMono =
+            saveMono.flatMap {
+                walletRepository
+                    .findByCreationDateBetweenAndStatusOrderByUpdateDateAsc(
+                        startDate,
+                        endDate,
+                        WALLET_CREATED_STATUS,
+                        walletSearchConfig.limit
+                    )
+                    .collectList()
             }
+
+        StepVerifier.create(walletsMono)
+            .assertNext { list -> assertEquals(2, list.size) }
             .verifyComplete()
     }
 }
