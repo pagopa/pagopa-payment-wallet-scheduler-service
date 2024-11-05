230f0bc72502065e04ea53f67723dc0f9737052b
e.serrao <emanuele.serrao@bvtech.com>
2024-11-05 12:51:10 +0100

    minor refactor

--- docker-local/azurite/create-queues.sh
+++ docker-local/azurite/create-queues.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+az storage queue create -n pagopa-wallet-expiration-queue --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
 az storage queue create -n pagopa-wallet-cdc-queue --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
 
 while true; do sleep 1; done
