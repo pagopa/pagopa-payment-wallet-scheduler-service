ae1fad025d1d34a977186da9ff57c3a4262a90c8
scaminati-bv <simone.caminati@bvtech.com>
2024-11-06 17:18:32 +0100

    feat: add test for lock-unlock errors

--- src/test/kotlin/it/pagopa/wallet/scheduler/service/SchedulerLockServiceTest.kt
+++ src/test/kotlin/it/pagopa/wallet/scheduler/service/SchedulerLockServiceTest.kt
@@ -1,29 +1,58 @@
 package it.pagopa.wallet.scheduler.service
 
 import it.pagopa.wallet.scheduler.config.properties.RedisJobLockPolicyConfig
+import it.pagopa.wallet.scheduler.exceptions.LockNotAcquiredException
+import it.pagopa.wallet.scheduler.exceptions.LockNotReleasedException
 import kotlin.test.Test
 import org.junit.jupiter.api.BeforeAll
+import org.mockito.kotlin.any
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
 import org.redisson.Redisson
+import org.redisson.api.RLockReactive
 import org.redisson.api.RedissonReactiveClient
 import org.redisson.config.Config
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import reactor.test.StepVerifier
+import reactor.core.publisher.Mono
+import reactor.kotlin.test.test
 import redis.embedded.RedisServer
 
 class SchedulerLockServiceTest {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
+    private val rLockReactive: RLockReactive = mock()
 
     @Test
     fun `Should acquire lock and release after work`() {
         val jobName = "job-name"
-        val result =
-            schedulerLockService
-                .acquireJobLock(jobName)
-                .doOnSuccess { logger.info("Simulate job work") }
-                .then(schedulerLockService.releaseJobLock(jobName))
 
-        StepVerifier.create(result).expectComplete().verify()
+        schedulerLockService
+            .acquireJobLock(jobName)
+            .doOnSuccess { logger.info("Simulate job work") }
+            .then(schedulerLockService.releaseJobLock(jobName))
+            .test()
+            .expectComplete()
+            .verify()
+    }
+
+    @Test
+    fun `Should throw LockNotAcquiredException when lock is already aquired`() {
+        given { rLockReactive.tryLock(any(), any(), any()) }.willReturn(Mono.just(false))
+
+        schedulerLockService
+            .acquireJobLock("job-name")
+            .test()
+            .expectError(LockNotAcquiredException::class.java)
+    }
+
+    @Test
+    fun `Should throw LockNotReleasedException when fail lock release`() {
+        given { rLockReactive.unlock() }.willThrow(RuntimeException::class.java)
+
+        schedulerLockService
+            .releaseJobLock("job-name")
+            .test()
+            .expectError(LockNotReleasedException::class.java)
     }
 
     companion object {
