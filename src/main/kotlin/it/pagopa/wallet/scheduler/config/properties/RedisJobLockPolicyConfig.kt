136203337d406b45582f6c7804f1dabd491e8cc9
scaminati-bv <simone.caminati@bvtech.com>
2024-11-07 10:03:39 +0100

    fix: change redis lock name

--- src/main/kotlin/it/pagopa/wallet/scheduler/config/properties/RedisJobLockPolicyConfig.kt
+++ src/main/kotlin/it/pagopa/wallet/scheduler/config/properties/RedisJobLockPolicyConfig.kt
@@ -5,5 +5,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "scheduler.redis-job-lock")
 data class RedisJobLockPolicyConfig(val keyspace: String, val ttlSec: Long, val waitTimeSec: Long) {
 
-    fun getLockByTarget(target: String): String = "%s:%s:%s".format(keyspace, "job", target)
+    fun getLockByTarget(target: String): String = "%s:%s:%s".format(keyspace, "lock", target)
 }
