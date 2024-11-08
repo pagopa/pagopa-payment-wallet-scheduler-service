d6f7997b97eb0489ba1609bcbd9cbf9067a13472
scaminati-bv <simone.caminati@bvtech.com>
2024-11-08 10:45:37 +0100

    Revert "revert: scheduler job and lock integration"
    This reverts commit 0bc88cb48673239610451af9c78c6e11de95b79e.

--- src/main/kotlin/it/pagopa/wallet/scheduler/scheduledjob/PaymentWalletScheduledJob.kt
+++ src/main/kotlin/it/pagopa/wallet/scheduler/scheduledjob/PaymentWalletScheduledJob.kt
@@ -3,6 +3,7 @@ package it.pagopa.wallet.scheduler.scheduledjob
 import it.pagopa.wallet.scheduler.config.properties.PaymentWalletJobConfiguration
 import it.pagopa.wallet.scheduler.jobs.config.OnboardedPaymentWalletJobConfiguration
 import it.pagopa.wallet.scheduler.jobs.paymentwallet.OnboardedPaymentWalletJob
+import it.pagopa.wallet.scheduler.service.SchedulerLockService
 import java.time.Duration
 import java.time.Instant
 import org.slf4j.LoggerFactory
@@ -13,34 +14,44 @@ import org.springframework.stereotype.Service
 @Service
 class PaymentWalletScheduledJob(
     @Autowired private val onboardedPaymentWalletJob: OnboardedPaymentWalletJob,
-    @Autowired private val paymentWalletJobConfiguration: PaymentWalletJobConfiguration
+    @Autowired private val paymentWalletJobConfiguration: PaymentWalletJobConfiguration,
+    @Autowired private val schedulerLockService: SchedulerLockService
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @Scheduled(cron = "\${payment-wallet-job.execution.cron}")
     fun processOnboardedPaymentWallets() {
         val startTime = Instant.now()
-        onboardedPaymentWalletJob
-            .process(
-                OnboardedPaymentWalletJobConfiguration(
-                    startDate = paymentWalletJobConfiguration.startDate,
-                    endDate = paymentWalletJobConfiguration.endDate
-                )
-            )
-            .subscribe(
-                {
-                    logger.info(
-                        "Payment wallets processed successfully. Latest created date processed wallet: [{}]",
-                        it
+        schedulerLockService
+            .acquireJobSemaphore(jobName = onboardedPaymentWalletJob.id())
+            .doOnError { logger.error("Unable to start job without semaphore acquiring", it) }
+            .flatMap {
+                val semaphoreId = it
+                onboardedPaymentWalletJob
+                    .process(
+                        OnboardedPaymentWalletJobConfiguration(
+                            startDate = paymentWalletJobConfiguration.startDate,
+                            endDate = paymentWalletJobConfiguration.endDate
+                        )
                     )
-                },
-                { logger.error("Exception processing payment wallets", it) },
-                {
-                    logger.info(
-                        "Overall processing completed. Elapsed time: [{}]",
-                        Duration.between(startTime, Instant.now())
-                    )
-                }
-            )
+                    .doOnSuccess {
+                        logger.info(
+                            "Payment wallets processed successfully. Latest created date processed wallet: [{}]",
+                            it
+                        )
+                    }
+                    .doOnError { logger.error("Exception processing payment wallets", it) }
+                    .doFinally {
+                        logger.info(
+                            "Overall processing completed. Elapsed time: [{}]",
+                            Duration.between(startTime, Instant.now())
+                        )
+                        schedulerLockService.releaseJobSemaphore(
+                            jobName = onboardedPaymentWalletJob.id(),
+                            semaphoreId = semaphoreId
+                        )
+                    }
+            }
+            .subscribe()
     }
 }
--- src/test/kotlin/it/pagopa/wallet/scheduler/scheduledjob/PaymentWalletScheduledJobTest.kt
+++ src/test/kotlin/it/pagopa/wallet/scheduler/scheduledjob/PaymentWalletScheduledJobTest.kt
@@ -3,17 +3,20 @@ package it.pagopa.wallet.scheduler.scheduledjob
 import it.pagopa.wallet.scheduler.config.properties.PaymentWalletJobConfiguration
 import it.pagopa.wallet.scheduler.jobs.config.OnboardedPaymentWalletJobConfiguration
 import it.pagopa.wallet.scheduler.jobs.paymentwallet.OnboardedPaymentWalletJob
+import it.pagopa.wallet.scheduler.service.SchedulerLockService
 import java.time.Duration
 import java.time.Instant
 import kotlinx.coroutines.reactor.mono
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.*
+import reactor.core.publisher.Hooks
 import reactor.core.publisher.Mono
 
 class PaymentWalletScheduledJobTest {
     private val startDate = Instant.now()
     private val onboardedPaymentWalletJob: OnboardedPaymentWalletJob = mock()
+    private val schedulerLockService: SchedulerLockService = mock()
     private val paymentWalletJobConfiguration =
         PaymentWalletJobConfiguration(
             startDate = startDate,
@@ -23,14 +26,21 @@ class PaymentWalletScheduledJobTest {
     private val paymentWalletScheduledJob =
         PaymentWalletScheduledJob(
             onboardedPaymentWalletJob = onboardedPaymentWalletJob,
-            paymentWalletJobConfiguration = paymentWalletJobConfiguration
+            paymentWalletJobConfiguration = paymentWalletJobConfiguration,
+            schedulerLockService = schedulerLockService
         )
 
     @Test
     fun `Should execute batch successfully`() {
         // pre-requisites
+        val jobId = "jobId"
         given(onboardedPaymentWalletJob.process(configuration = any()))
             .willReturn(mono { Instant.now().toString() })
+        given(onboardedPaymentWalletJob.id()).willReturn(jobId)
+        given(schedulerLockService.acquireJobSemaphore(any())).willReturn(mono { "semaphore-id" })
+        given(schedulerLockService.releaseJobSemaphore(any(), any())).willReturn(null)
+
+        Hooks.onOperatorDebug()
 
         // Test
         paymentWalletScheduledJob.processOnboardedPaymentWallets()
@@ -43,6 +53,7 @@ class PaymentWalletScheduledJobTest {
                         endDate = paymentWalletJobConfiguration.endDate
                     )
             )
+        verify(schedulerLockService, times(1)).acquireJobSemaphore(jobId)
     }
 
     @Test
