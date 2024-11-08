a4779289b12168e1023a2fd733843660ab5611e2
e.serrao <emanuele.serrao@bvtech.com>
2024-11-08 11:50:33 +0100

    updated tests

--- docker-local/mongo/mongo-init.js
+++ docker-local/mongo/mongo-init.js
@@ -38,4 +38,36 @@ db.getCollection('payment-wallets').insertMany([{
   "updateDate": "2024-06-21T10:34:18.850935581Z",
   "onboardingChannel": "IO",
   "_class": "it.pagopa.wallet.documents.wallets.Wallet"
-}]);
+},{
+    "_id": "a0a27446-e6d8-47d2-8786-ca393f19c30b",
+    "userId": "6fd46190-f4bf-4a9f-9e70-fb98f235a449",
+    "status": "VALIDATED",
+    "paymentMethodId": "9d735400-9450-4f7e-9431-8c1e7fa2a339",
+    "contractId": "W1729763462700lj43",
+    "validationOperationResult": "EXECUTED",
+    "applications": [
+      {
+        "_id": "PAGOPA",
+        "status": "ENABLED",
+        "creationDate": "2024-10-24T09:50:49.292579182Z",
+        "updateDate": "2024-10-24T09:50:49.292579682Z",
+        "metadata": {}
+      }
+    ],
+    "details": {
+      "maskedEmail": "b***@icbpi.it",
+      "pspId": "BCITITMM",
+      "pspBusinessName": "Intesa Sanpaolo S.p.A",
+      "_class": "it.pagopa.wallet.documents.wallets.details.PayPalDetails"
+    },
+    "clients": {
+      "IO": {
+        "status": "ENABLED"
+      }
+    },
+    "version": 3,
+    "creationDate": "2024-10-24T09:50:49.995798539Z",
+    "updateDate": "2024-10-24T09:51:22.226817657Z",
+    "onboardingChannel": "IO",
+    "_class": "it.pagopa.wallet.documents.wallets.Wallet"
+  }]);
