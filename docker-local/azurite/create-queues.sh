b473381009bc92de4fb3683e3eed175cc855f810
EmanueleBVtech <emanuele.serrao@bvtech.com>
2024-11-08 14:49:34 +0100

    Feat [CHK-3522] Added integration test in code review pipeline (#8)
    * fixed prop
    
    * updated docker compose and pipeline
    
    * bugfix
    
    * update
    
    * added cdc
    
    * fixed docker compose
    
    * minor refactor
    
    * minor fix
    
    * updated port
    
    * updated element
    
    * updated collection name
    
    * fixed code review
    
    * added missing env vars
    
    * minor fix
    
    * minor update
    
    * fix: docker compose configuration
    
    * restored data.json
    
    * restored data.json
    
    * fix test
    
    * formatting fix
    
    * fixed id reading
    
    * fixed id reading
    
    * updated tests
    
    * fixed path
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * fixed sorting of creation date
    
    * added safety sleep
    
    * fixed log
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * removed excessive prints
    
    * json now should be on one line
    
    ---------
    
    Co-authored-by: Pietro Tota <pietro.tota@pagopa.it>

--- docker-local/azurite/create-queues.sh
+++ docker-local/azurite/create-queues.sh
@@ -2,4 +2,3 @@
 
 az storage queue create -n pagopa-wallet-cdc-queue --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
 
-while true; do sleep 1; done
