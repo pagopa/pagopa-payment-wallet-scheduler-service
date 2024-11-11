0d9f078ba3656f7000302679bc8742af6570930c
scaminati-bv <simone.caminati@bvtech.com>
2024-11-11 10:02:43 +0100

    feat: add unit test for job lock

--- src/test/kotlin/it/pagopa/wallet/scheduler/scheduledjob/PaymentWalletScheduledJobTest.kt
+++ src/test/kotlin/it/pagopa/wallet/scheduler/scheduledjob/PaymentWalletScheduledJobTest.kt
@@ -1,17 +1,17 @@
 package it.pagopa.wallet.scheduler.scheduledjob
 
 import it.pagopa.wallet.scheduler.config.properties.PaymentWalletJobConfiguration
+import it.pagopa.wallet.scheduler.exceptions.SemNotAcquiredException
 import it.pagopa.wallet.scheduler.jobs.config.OnboardedPaymentWalletJobConfiguration
 import it.pagopa.wallet.scheduler.jobs.paymentwallet.OnboardedPaymentWalletJob
 import it.pagopa.wallet.scheduler.service.SchedulerLockService
-import java.time.Duration
-import java.time.Instant
 import kotlinx.coroutines.reactor.mono
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.*
-import reactor.core.publisher.Hooks
 import reactor.core.publisher.Mono
+import java.time.Duration
+import java.time.Instant
 
 class PaymentWalletScheduledJobTest {
     private val startDate = Instant.now()
@@ -34,17 +34,17 @@ class PaymentWalletScheduledJobTest {
     fun `Should execute batch successfully`() {
         // pre-requisites
         val jobId = "jobId"
+        val semaphoreId = "semaphore-id"
         given(onboardedPaymentWalletJob.process(configuration = any()))
             .willReturn(mono { Instant.now().toString() })
         given(onboardedPaymentWalletJob.id()).willReturn(jobId)
-        given(schedulerLockService.acquireJobSemaphore(any())).willReturn(mono { "semaphore-id" })
+        given(schedulerLockService.acquireJobSemaphore(any())).willReturn(mono { semaphoreId })
         given(schedulerLockService.releaseJobSemaphore(any(), any())).willReturn(null)
-
-        Hooks.onOperatorDebug()
 
         // Test
         paymentWalletScheduledJob.processOnboardedPaymentWallets()
         // verifications
+        verify(onboardedPaymentWalletJob, times(1)).id()
         verify(onboardedPaymentWalletJob, after(1000).times(1))
             .process(
                 configuration =
@@ -54,17 +54,24 @@ class PaymentWalletScheduledJobTest {
                     )
             )
         verify(schedulerLockService, times(1)).acquireJobSemaphore(jobId)
+        verify(schedulerLockService, times(1)).releaseJobSemaphore(jobId, semaphoreId)
     }
 
     @Test
     fun `Should handle processing exception`() {
         // pre-requisites
+        val jobId = "jobId"
+        val semaphoreId = "semaphore-id"
         given(onboardedPaymentWalletJob.process(configuration = any()))
             .willReturn(Mono.error(RuntimeException("Exception during job execution")))
+        given(onboardedPaymentWalletJob.id()).willReturn(jobId)
+        given(schedulerLockService.acquireJobSemaphore(any())).willReturn(mono { semaphoreId })
+        given(schedulerLockService.releaseJobSemaphore(any(), any())).willReturn(null)
 
         // Test
         assertDoesNotThrow { paymentWalletScheduledJob.processOnboardedPaymentWallets() }
         // verifications
+        verify(onboardedPaymentWalletJob, times(1)).id()
         verify(onboardedPaymentWalletJob, after(1000).times(1))
             .process(
                 configuration =
@@ -73,5 +80,23 @@ class PaymentWalletScheduledJobTest {
                         endDate = paymentWalletJobConfiguration.endDate
                     )
             )
+        verify(schedulerLockService, times(1)).acquireJobSemaphore(jobId)
+        verify(schedulerLockService, times(1)).releaseJobSemaphore(jobId, semaphoreId)
+    }
+
+    @Test
+    fun `Should skip batch execution if don't acquire lock`() {
+        // pre-requisites
+        val jobId = "jobId"
+        given(onboardedPaymentWalletJob.id()).willReturn(jobId)
+        given(schedulerLockService.acquireJobSemaphore(any())).willReturn(Mono.error(SemNotAcquiredException("jobName")))
+
+        // Test
+        paymentWalletScheduledJob.processOnboardedPaymentWallets()
+
+        // verifications
+        verify(onboardedPaymentWalletJob, times(1)).id()
+        verify(schedulerLockService, times(1)).acquireJobSemaphore(jobId)
+        verify(onboardedPaymentWalletJob, times(0)).process(any())
     }
 }
