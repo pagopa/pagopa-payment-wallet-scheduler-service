1c506f9883337e16f6d3ffa2e57dfdc5a72ac832
CianoDanilo <danilo.ciano@pagopa.it>
2024-11-11 17:00:31 +0100

    feat(semaphore): fix release semaphore

--- src/main/kotlin/it/pagopa/wallet/scheduler/scheduledjob/PaymentWalletScheduledJob.kt
+++ src/main/kotlin/it/pagopa/wallet/scheduler/scheduledjob/PaymentWalletScheduledJob.kt
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 
 @Service
 class PaymentWalletScheduledJob(
@@ -45,11 +46,15 @@ class PaymentWalletScheduledJob(
                             "Overall processing completed. Elapsed time: [{}]",
                             Duration.between(startTime, Instant.now())
                         )
-                        schedulerLockService.releaseJobSemaphore(
-                            jobName = onboardedPaymentWalletJob.id(),
-                            semaphoreId = semaphoreId
-                        )
                     }
+                    .onErrorResume { Mono.empty() }
+                    .thenReturn(semaphoreId)
+            }
+            .flatMap {
+                schedulerLockService.releaseJobSemaphore(
+                    jobName = onboardedPaymentWalletJob.id(),
+                    semaphoreId = it
+                )
             }
             .subscribe()
     }
