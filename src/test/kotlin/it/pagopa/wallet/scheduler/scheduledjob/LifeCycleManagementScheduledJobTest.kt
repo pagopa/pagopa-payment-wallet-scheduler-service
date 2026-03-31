40183c167eca0473db6d916840f455d36f95927a
Stefano Rondinella <stefrondinella@live.it>
2026-03-31 17:36:44 +0200

    refactor: spotless apply

--- src/test/kotlin/it/pagopa/wallet/scheduler/scheduledjob/LifeCycleManagementScheduledJobTest.kt
+++ src/test/kotlin/it/pagopa/wallet/scheduler/scheduledjob/LifeCycleManagementScheduledJobTest.kt
@@ -4,6 +4,7 @@ import it.pagopa.wallet.scheduler.config.properties.LifecycleManagementConfigura
 import it.pagopa.wallet.scheduler.exceptions.SemNotAcquiredException
 import it.pagopa.wallet.scheduler.jobs.lifecyclemanagement.UpdateTtlWalletJob
 import it.pagopa.wallet.scheduler.service.SchedulerLockService
+import kotlin.test.Test
 import kotlinx.coroutines.reactor.mono
 import org.mockito.kotlin.after
 import org.mockito.kotlin.any
@@ -12,44 +13,24 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import reactor.core.publisher.Mono
-import kotlin.test.Test
 
 class LifeCycleManagementScheduledJobTest {
 
     private val updateTtlWalletJob: UpdateTtlWalletJob = mock()
-    private val jobConfiguration: LifecycleManagementConfiguration = LifecycleManagementConfiguration(90)   // 3 mounth excluded
+    private val jobConfiguration: LifecycleManagementConfiguration =
+        LifecycleManagementConfiguration(90) // 3 mounth excluded
     private val schedulerLockService: SchedulerLockService = mock()
 
-   private val lifeCycleManagementScheduledJob : LifeCycleManagementScheduledJob =
-       LifeCycleManagementScheduledJob(updateTtlWalletJob,jobConfiguration,schedulerLockService)
-
-   @Test
-   fun `Should execute batch successfully`() {
-       val jobId = "jobId"
-       val semaphoreId = "semaphore-id"
-       given(schedulerLockService.acquireJobSemaphore(any())).willReturn(mono { semaphoreId })
-       given(schedulerLockService.releaseJobSemaphore(any(), any())).willReturn(null)
-       given(updateTtlWalletJob.process(any())).willReturn(Mono.just<Int>(10))
-       given(updateTtlWalletJob.id()).willReturn(jobId)
-
-       // Test the process
-       lifeCycleManagementScheduledJob.processLifeCycleWallets()
-
-       verify(updateTtlWalletJob, after(1000).times(2)).id()
-       verify(updateTtlWalletJob, after(1000).times(1)).process(any())
-
-       verify(schedulerLockService,times(1)).acquireJobSemaphore(jobId)
-       verify(schedulerLockService,times(1)).releaseJobSemaphore(jobId,semaphoreId)
-
-   }
+    private val lifeCycleManagementScheduledJob: LifeCycleManagementScheduledJob =
+        LifeCycleManagementScheduledJob(updateTtlWalletJob, jobConfiguration, schedulerLockService)
 
     @Test
-    fun `Should handle process exception during the process`() {
+    fun `Should execute batch successfully`() {
         val jobId = "jobId"
         val semaphoreId = "semaphore-id"
         given(schedulerLockService.acquireJobSemaphore(any())).willReturn(mono { semaphoreId })
         given(schedulerLockService.releaseJobSemaphore(any(), any())).willReturn(null)
-        given(updateTtlWalletJob.process(any())).willReturn(Mono.error(RuntimeException("Error during the process")))
+        given(updateTtlWalletJob.process(any())).willReturn(Mono.just<Int>(10))
         given(updateTtlWalletJob.id()).willReturn(jobId)
 
         // Test the process
@@ -58,19 +39,39 @@ class LifeCycleManagementScheduledJobTest {
         verify(updateTtlWalletJob, after(1000).times(2)).id()
         verify(updateTtlWalletJob, after(1000).times(1)).process(any())
 
-        verify(schedulerLockService,times(1)).acquireJobSemaphore(jobId)
-        verify(schedulerLockService,times(1)).releaseJobSemaphore(jobId,semaphoreId)
-
+        verify(schedulerLockService, times(1)).acquireJobSemaphore(jobId)
+        verify(schedulerLockService, times(1)).releaseJobSemaphore(jobId, semaphoreId)
     }
 
+    @Test
+    fun `Should handle process exception during the process`() {
+        val jobId = "jobId"
+        val semaphoreId = "semaphore-id"
+        given(schedulerLockService.acquireJobSemaphore(any())).willReturn(mono { semaphoreId })
+        given(schedulerLockService.releaseJobSemaphore(any(), any())).willReturn(null)
+        given(updateTtlWalletJob.process(any()))
+            .willReturn(Mono.error(RuntimeException("Error during the process")))
+        given(updateTtlWalletJob.id()).willReturn(jobId)
+
+        // Test the process
+        lifeCycleManagementScheduledJob.processLifeCycleWallets()
+
+        verify(updateTtlWalletJob, after(1000).times(2)).id()
+        verify(updateTtlWalletJob, after(1000).times(1)).process(any())
+
+        verify(schedulerLockService, times(1)).acquireJobSemaphore(jobId)
+        verify(schedulerLockService, times(1)).releaseJobSemaphore(jobId, semaphoreId)
+    }
 
     @Test
     fun `Should not call process if the lock is not acquired`() {
         val jobId = "jobId"
         val semaphoreId = "semaphore-id"
-        given(schedulerLockService.acquireJobSemaphore(any())).willReturn(Mono.error(SemNotAcquiredException("jobName")))
+        given(schedulerLockService.acquireJobSemaphore(any()))
+            .willReturn(Mono.error(SemNotAcquiredException("jobName")))
         given(schedulerLockService.releaseJobSemaphore(any(), any())).willReturn(null)
-        given(updateTtlWalletJob.process(any())).willReturn(Mono.error(RuntimeException("Error during the process")))
+        given(updateTtlWalletJob.process(any()))
+            .willReturn(Mono.error(RuntimeException("Error during the process")))
         given(updateTtlWalletJob.id()).willReturn(jobId)
 
         // Test the process
@@ -79,10 +80,7 @@ class LifeCycleManagementScheduledJobTest {
         verify(updateTtlWalletJob, after(1000).times(1)).id()
         verify(updateTtlWalletJob, after(1000).times(0)).process(any())
 
-        verify(schedulerLockService,times(1)).acquireJobSemaphore(jobId)
-        verify(schedulerLockService,times(0)).releaseJobSemaphore(jobId,semaphoreId)
-
+        verify(schedulerLockService, times(1)).acquireJobSemaphore(jobId)
+        verify(schedulerLockService, times(0)).releaseJobSemaphore(jobId, semaphoreId)
     }
-
-
 }
